51182c8011c46ee3d615f5bcfe80a4070e0ed9f3
justitow <justitow@gmail.com>
2016-02-12 21:08:57 -0800

    Added memory.cpp and memory.h file

---
+++ memory.cpp
@@ -0,0 +1,9 @@
+//
+//  memory.cpp
+//  ECS_040_Program_05
+//
+//  Created by Justin Weich on 2/12/16.
+//  Copyright © 2016 Justin Weich. All rights reserved.
+//
+
+#include "memory.h"
---
+++ memory.h
@@ -0,0 +1,14 @@
+//
+//  memory.h
+//  ECS_040_Program_05
+//
+//  Created by Justin Weich on 2/12/16.
+//  Copyright © 2016 Justin Weich. All rights reserved.
+//
+
+#ifndef memory_h
+#define memory_h
+
+#include <stdio.h>
+
+#endif /* memory_h */
