fdbc783252d4d0f22ed5d2fd4cb09819fd32f9c7
justitow <justitow@gmail.com>
2016-02-17 08:50:29 -0800

    added the rest of the test files

--- test3.s
+++ test3.s
@@ -1,4 +1,4 @@
-	.file	"test4.c"
+	.file	"test2.c"
 	.text
 	.globl	main
 	.type	main, @function
@@ -10,31 +10,15 @@ main:
 	.cfi_offset 5, -8
 	movl	%esp, %ebp
 	.cfi_def_cfa_register 5
-	subl	$64, %esp
-	movl	$197, -12(%ebp)
-	cmpl	$99, -12(%ebp)
-	jg	.L2
-	movl	$134, -4(%ebp)
-	jmp	.L3
-.L2:
-	movl	$26, -4(%ebp)
-.L3:
-	movl	$0, -8(%ebp)
-	jmp	.L4
-.L5:
-	movl	-12(%ebp), %edx
+	subl	$16, %esp
+	movl	$197, -4(%ebp)
 	movl	-4(%ebp), %eax
-	addl	%eax, %edx
+	addl	$8, %eax
+	movl	%eax, -8(%ebp)
 	movl	-8(%ebp), %eax
-	movl	%edx, -52(%ebp,%eax,4)
-	addl	$1, -8(%ebp)
-.L4:
-	cmpl	$2, -8(%ebp)
-	jle	.L5
-	movl	-44(%ebp), %eax
-	addl	$18, %eax
+	subl	$4, %eax
 	movl	%eax, -12(%ebp)
-	movl	-4(%ebp), %eax
+	movl	-8(%ebp), %eax
 	leave
 	.cfi_restore 5
 	.cfi_def_cfa 4, 4
